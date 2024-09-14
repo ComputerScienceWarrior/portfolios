114168d0f6f9386ac7be0d17e3c0e042b24360e5
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-09-14 15:10:12 -0700

    adds data to user migration

--- db/migrate/001_create_users.rb
+++ db/migrate/001_create_users.rb
@@ -1,7 +1,19 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
-
+      t.string :first_name
+      t.string :last_name
+      t.string :username
+      t.string :email
+      t.string :phone_number
+      t.string :address_line_1
+      t.string :address_line_2
+      t.string :address_line_3
+      t.string :zipcode
+      t.string :state
+      t.string :country
+      t.string :password_digest
+      
       t.timestamps
     end
   end
