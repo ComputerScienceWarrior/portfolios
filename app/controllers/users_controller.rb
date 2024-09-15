bf028fec20775f54ed4f20733ad754f423068d1a
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-09-15 07:18:06 -0700

    creates users_controller with necessary CRUD actions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,51 @@
+class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      session[:user_id] = user.id
+      redirect_to user_path user
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :username, :email, :phone_number, :address_line_1, :address_line_2, :address_line_3, :zipcode, :state, :country, :password, :avatar_url)
+  end
+end
