e92fa949111e9f5f43fe4874e414f2952332f29e
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-09-20 09:14:48 -0700

    adds routing for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   resources :portfolios do
     resources :projects, only: [:index, :show, :new, :edit, :update, :destroy]
   end
+
+  get '/login', to: 'sessions#new'
+  get '/post', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
