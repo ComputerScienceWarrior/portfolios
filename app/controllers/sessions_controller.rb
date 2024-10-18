76025a75ed4df968e62c2c62f8d13fa9d973d4b0
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-10-18 15:36:48 -0700

    fix 'find_by" in create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(params[:user][:email])
+    user = User.find_by(username: params[:user][:username])
+    
     if user && user.authenticate(params[:user][:password])
       session[:user_id] = user.id
-      redirect_to user_path user
+      redirect_to user_path(user)
     else
       render :new
     end
